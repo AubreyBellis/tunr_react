bdd07f4f4a36e0db538e9bcb0f5e7e32be8e5637
AubreyBellis <contactaubreybellis@gmail.com>
2017-09-05 12:09:41 -0400

    artist and song controller done

---
+++ app/controllers/api/songs_controller.rb
@@ -0,0 +1,51 @@
+class Api::SongsController < ApplicationController
+  def index
+    @songs = Song.all
+    render json: @songs
+  end
+
+  def show
+    @song = Song.find(params[:id])
+    render json: @song
+  end
+
+  def create
+    @song = Song.new song_params
+
+    if @song.save
+      render json: @song
+    else
+      render json: {
+        message: 'Error when creating Song'
+      }
+    end
+  end
+
+  def update
+    @song = Song.find params[:id]
+
+    if @song.update(song_params)
+      render json: @song
+    else
+      render json: {
+        message: 'Error when updating Song'
+      }
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+
+    render json: {
+      message: 'Song successfully destroyed'
+    }
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :album, :photo_url, :artist)
+  end
+end
+  
